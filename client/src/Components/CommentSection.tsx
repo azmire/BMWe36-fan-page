e803dda4f7c643675beb818eb44238f59d65a7b6
Andelo Zmire <angelo.zmire@hotmail.com>
2024-03-27 16:38:57 +0100

    add and update comment added

--- client/src/Components/CommentSection.tsx
+++ client/src/Components/CommentSection.tsx
@@ -1,24 +1,78 @@
-import { Form, Toast } from "react-bootstrap";
+import { Button, Form, Image, Toast } from "react-bootstrap";
 import { CommentsectionType } from "../types/commentTypes";
 import { Comment } from "../types/dataTypes";
+import { useState } from "react";
 
-function CommentSection({ display, comments }: CommentsectionType) {
+function CommentSection({ display, comments, id }: CommentsectionType) {
+  const [existingComments, setExistingComments] = useState(comments);
+  const [newComment, setNewComment] = useState("");
+
+  console.log("comments :>> ", comments);
+
+  const userId = localStorage.getItem("userId");
+
+  const addComment = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("comment :>> ", newComment);
+    if (!newComment || !userId) {
+      console.log("no comment");
+      return;
+    } else {
+      const myHeaders = new Headers();
+      myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+
+      const urlencoded = new URLSearchParams();
+      urlencoded.append("comment", newComment);
+      urlencoded.append("author", userId);
+      urlencoded.append("post", id);
+
+      const requestOptions = {
+        method: "POST",
+        headers: myHeaders,
+        body: urlencoded,
+        redirect: "follow" as RequestRedirect,
+      };
+      try {
+        const response = await fetch(
+          "http://localhost:9876/api/comments/addcomment",
+          requestOptions
+        );
+
+        if (response.ok) {
+          const result = await response.json();
+          setExistingComments(result.postItem.comments);
+          setNewComment("");
+          console.log(result);
+        }
+        if (!response.ok) {
+          const result = await response.json();
+          console.log("result :>> ", result);
+        }
+      } catch (error) {
+        console.log("error :>> ", error);
+      }
+    }
+  };
+
   return (
     <>
+      {/* *************** DIDSPLAY COMMENTS *************** */}
       <div className={display}>
-        {comments.map((comment: Comment) => {
-          console.log("comment :>> ", comment);
+        {existingComments.map((comment: Comment) => {
           return (
-            <div>
+            <div key={comment.createdAt}>
               <div className="border-top m-2">
                 <Toast className="w-100">
                   <Toast.Header>
-                    <img
-                      src="holder.js/20x20?text=%20"
-                      className="rounded me-2"
-                      alt=""
+                    <Image
+                      //user image rounded
+                      style={{ height: "3vh", width: "3vh", border: "5px" }}
+                      src={comment.author.avatar}
+                      roundedCircle
                     />
-                    <strong className="me-auto">Author</strong>
+                    <strong className="me-auto">
+                      {comment.author.username}
+                    </strong>
                     <small>{comment.createdAt}</small>
                   </Toast.Header>
                   <Toast.Body>{comment.comment}</Toast.Body>
@@ -27,28 +81,31 @@ function CommentSection({ display, comments }: CommentsectionType) {
             </div>
           );
         })}
-        {/* write a comment */}
+        {/* *************** WRITE A COMMENT*************** */}
         <div className="border-top m-2">
-          <Toast className="w-100">
-            <Toast.Header>
-              <img
-                src="holder.js/20x20?text=%20"
-                className="rounded me-2"
-                alt=""
+          <Form
+            onSubmit={(e) => {
+              addComment(e);
+            }}
+          >
+            <Form.Group
+              className="mb-3"
+              controlId="exampleForm.ControlTextarea1"
+            >
+              <Form.Label></Form.Label>
+              <Form.Control
+                placeholder="Write a comment..."
+                as="textarea"
+                rows={3}
+                value={newComment}
+                onChange={(e) => {
+                  setNewComment(e.target.value);
+                }}
               />
-              <strong className="me-auto">Write a comment</strong>
-              <small>Date</small>
-            </Toast.Header>
-            <Form>
-              <Form.Group
-                className="mb-3"
-                controlId="exampleForm.ControlTextarea1"
-              >
-                <Form.Label>Example textarea</Form.Label>
-                <Form.Control as="textarea" rows={3} />
-              </Form.Group>
-            </Form>
-          </Toast>
+            </Form.Group>
+
+            <Button type="submit">Submit</Button>
+          </Form>
         </div>
       </div>
     </>
